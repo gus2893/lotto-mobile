4c77a378e423421e1cc640aaeab479036946598e
Gustavo Evangelista <gus2893@gmail.com>
2023-08-30 22:08:40 -0400

    app set up

---
+++ lib/views/onboarding.dart
@@ -0,0 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
+import 'package:flutter/material.dart';
+
+class BackgroundImageFb1 extends StatelessWidget {
+  final Widget child;
+  final String imageUrl;
+  const BackgroundImageFb1(
+      {required this.child, required this.imageUrl, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // Place as the child widget of a scaffold
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(imageUrl),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: child,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final usersQuery = FirebaseFirestore.instance.collection('lotteries');
+
+  @override
+  Widget build(BuildContext context) {
+    return BackgroundImageFb1(
+      imageUrl:
+          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+      child: FirestoreListView<Map<String, dynamic>>(
+        query: usersQuery,
+        itemBuilder: (context, snapshot) {
+          Map<String, dynamic> user = snapshot.data();
+          return Text('User name is ${user['openTime']}');
+        },
+      ),
+    );
+  }
+}
